614b76fc15d23ac25b98ce27d16e4383eecae766
fasua samya <perfectseasongloballtd@gmail.com>
2026-06-29 02:55:40 +0100

    run sever and client together

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+  {
+    name: "server",
+    command: "python  manage.py runserver",
+    cwd: "packages/server",
+    prefixColor: "cyan",
+  },
+
+  {
+    name: "client",
+    command: "npm run dev",
+    cwd: "packages/client",
+    prefixColor: "green",
+  },
+]);
