c1f6b248d270a7614c7ca188426f48808fd68505
fasua samya <perfectseasongloballtd@gmail.com>
2026-07-05 19:08:16 +0100

    creating pages

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,34 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/ui/Footer';
+import ChatGateway from './components/ui/ChatGateway';
+import NavBar from './components/ui/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Properties from './pages/Properties';
 
-function App() {}
+function App() {
+   return (
+      <Router>
+         <div className="flex min-h-screen flex-col bg-white">
+            <NavBar />
+            <main className="flex-1 pt-16">
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="/properties" element={<Properties />} />
+               </Routes>
+               <section className="mx-auto w-full max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
+                  <div className="overflow-hidden rounded-2xl bg-white shadow-md transition duration-300 hover:shadow-xl">
+                     <ChatGateway />
+                  </div>
+               </section>
+            </main>
+            <Footer />
+         </div>
+      </Router>
+   );
+}
 
 export default App;
